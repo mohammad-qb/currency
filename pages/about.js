7cfb406c2b1e1e5db0d50fd05137b4443b1af1eb
mohammad ahamd <ma3571318z@gmail.com>
2020-05-16 16:54:00 +0300

    1st commit

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import Layout from "../components/Layout";
+
+const About = () => (
+  <Layout>
+    <h1>About the website</h1>
+    <p>Hello World! 😘 </p>
+  </Layout>
+);
+
+export default About;
